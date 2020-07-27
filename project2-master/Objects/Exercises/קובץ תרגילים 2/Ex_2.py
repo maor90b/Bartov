86ccc34556af1bfcc469ea07978806302146fd9f
raphaelhazout <68278696+raphaelhazout@users.noreply.github.com>
2020-07-27 16:36:31 +0300

    ,

---
+++ project2-master/Objects/Exercises/קובץ תרגילים 2/Ex_2.py
@@ -0,0 +1,12 @@
+class Bus:
+    def __init__(self,size):
+        self.size=size
+        self.passangers=0
+        self.listS=['free' for i in range(self.size)]
+
+
+    def getOn(self,name):
+
+
+
+
